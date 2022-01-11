228eaa8e8332f047168c679f3099808c6f05bbe5
dogefy <81802833+dogefy@users.noreply.github.com>
2022-01-11 21:47:14 +0800

    display correct name and improve

--- get_uid_from_name.py
+++ get_uid_from_name.py
@@ -3,28 +3,32 @@ import bs4
 import re
 
 headers = {
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36 Edg/97.0.1072.55'
+    'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36 Edg/97.0.1072.55"
 }
 
 
 def get_uid(name):
+    id = []
     url = 'https://search.bilibili.com/upuser?keyword={}'.format(name)
-    a = requests.get(url)
-    soup = bs4.BeautifulSoup(a.content, 'lxml')
-    match = re.search(r'space.bilibili.com/([0-9]+)', str(soup.select('.title')))
-    try:
-        match.group(0)
-    except AttributeError:
-        return -1
+    get_page = requests.get(url)
+    soup = bs4.BeautifulSoup(get_page.content, 'lxml')
+    uid_pattern = re.compile('space.bilibili.com/([0-9]+)')
+    uid_match = uid_pattern.findall(str(soup.select('.title')))
+    if len(uid_match) == 0:
+        id.append(-1)
+        return id
     else:
-        match = re.search(r'[0-9]+', str(match.group(0)))
-        return match.group(0)
+        name_pattern = re.compile('target="_blank" title="(.+?)"')
+        name_match = name_pattern.findall(str(soup.select('.title')))
+        id.append(uid_match[0])
+        id.append(name_match[0])
+        return id
 
 
 if __name__ == '__main__':
     name = input('Please input the name : ')
     uid = get_uid(name)
-    if uid == -1:
-        print('Can not find the user with similar name to {}.'.format(name))
+    if uid[0] == -1:
+        print("Can not find the user with similar name to '{}'.".format(name))
     else:
-        print('The UID of {} :'.format(name), uid)
+        print('The UID of {} : {}'.format(uid[1], uid[0]))
